b5f73a5c5277639d18479817f2ded15a09a39831
HuynhTri0811 <huynhhuutri0811@gmail.com>
2021-04-05 00:10:49 +0700

    update

---
+++ ERP_TEST_API/Controllers/UserControllers.cs
@@ -0,0 +1,52 @@
+﻿using ERP_TEST.Model.Model;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ERP_TEST_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserControllers : ControllerBase
+    {
+        // GET: api/<User>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            Part part = new Part()
+            {
+                Oid = Guid.NewGuid()
+            };
+            return Ok(part);
+        }
+
+        // GET api/<User>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<User>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<User>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<User>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
